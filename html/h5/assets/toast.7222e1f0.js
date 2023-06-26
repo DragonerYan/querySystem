728494550f5dfcef5ee775a832fd8d958c410f69
DragonerYan <2269938169@qq.com>
2023-06-26 14:10:45 +0800

    更新前台程序

---
+++ html/h5/assets/toast.7222e1f0.js
@@ -0,0 +1,1 @@
+import{A as o}from"./index-41510e59.js";const t=t=>{o({title:t,duration:2e3,icon:"none"})};export{t};
--- html/h5/assets/toast.e04529bf.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index-380bf995.js";const t=t=>{o({title:t,duration:2e3,icon:"none"})};export{t};
